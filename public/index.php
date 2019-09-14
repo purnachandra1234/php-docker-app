4f2e5c29523c8e75d772f48b37104baf60ffdc21
Thomas Baumann <thomas@thomasbaumann.me>
2019-09-14 16:37:51 +0200

    Made db host configurable

--- public/index.php
+++ public/index.php
@@ -3,11 +3,12 @@
 echo "<h1>Hello php world!</h1>";
 
 // Read mysql version
+$host = getenv('MYSQL_HOST');
 $db = getenv('MYSQL_DATABASE');
 $user = getenv('MYSQL_USER');
 $passwd = getenv('MYSQL_PASSWORD');
 
-$pdo = new PDO('mysql:host=db;dbname=' . $db, $user, $passwd);
+$pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $passwd);
 
 $stm = $pdo->query("SELECT VERSION()");
 list($mysqlVersion) = $stm->fetch();
